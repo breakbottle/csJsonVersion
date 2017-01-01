3a5965f918d7bbd86c153223d0d2dc03b509e055
Clint Small Cain <github@breakbottle.com>
2017-01-01 11:40:36 -0600

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# cs-json-version
+
+Updates the version of each provided *.json file then process a commit/tag and push
+## install
+
+~~~`npm install cs-json-version`~~~ haven't published to npm, quick deal for me
+
+  `npm install https://github.com/breakbottle/csJsonVersion.git`
+
+## use
+
+```javascript
+
+  var csVersion = require('cs-version');
+//long version
+  csVersion.changeVersion('../cs-config.json',params[3],function(v){
+      csVersion.changeVersion('package.json',params[3],function(version){
+          csVersion.commitDetails(version);
+          console.info('version added: ',version);
+      });
+  });
+//short quick version
+  csVersion.run(['../cs-config.json','package.json'],params[3]);
+```
+## author(s)
+  Clint W. Cain (Small)
+## summary
+
+This module updates the version number of all given *.json files, then adds a tag and commit and pushes to repo. If version number is provided in 0.1.0 Semantic Version, it will update with exact number provided. If no version is provided it will auto increment the patch up to a limit of 50, the minor to a limit of 10.
+
+> Note: git globals configs may need to be updated to use the git functionality. todo:)
+
+## license
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,92 @@
+/**
+ * Author: Clint Small Cain
+ * Date: 1/1/2017
+ * Time: 10:34 AM
+ * Description:
+ */
+var jsonfile = require('jsonfile');
+require('shelljs/global');
+
+var commitDetails = function(version){
+    exec('git add -u');
+    exec('git commit -a -m "v'+version+'"');
+    exec('git tag v'+version);
+    exec('git push --follow-tags origin master')
+
+};
+var makeVersion = function(fileVersion){//to be used with automation
+    var levels = fileVersion.split(".");//expects 0.0.0 Semantic Version
+    var patchLimit = 50;
+    var minorLimit = 10;
+
+    if(levels[2] <= patchLimit){
+        levels[2] = parseInt(levels[2])+1;
+    } else {
+        levels[2] = 0;
+        if(levels[1] < minorLimit){
+            levels[1] = parseInt(levels[1])+1;
+        } else {
+            levels[1] = 0;
+            levels[0]++;
+        }
+    }
+    return levels[0] + "." + levels[1] + "." + levels[2];
+};//
+var changeVersion = function (configFile,version,callback) {
+    jsonfile.readFile(configFile, function (err, obj) {
+        //calculate
+        if(!version){
+            version = makeVersion(obj.version);
+        }
+
+        //change tsConfig option on the fly, for builds
+        if(version){
+            obj.version = version;
+            jsonfile.writeFile(configFile, obj, function (err) {
+                if (err) {
+                    console.error(configFile+" json error: --->", (err || 'none'));
+                }
+                if(callback){
+                    callback(obj.version);
+                }
+                console.error(configFile+": json set: --->", obj.version)
+            });
+
+        }
+
+    });
+};//
+var count = 0;
+var called = function(listOfFiles,ver){
+    if(typeof listOfFiles[count] == 'string'){
+        changeVersion(listOfFiles[count],ver,function(v){
+            count++;
+            called(listOfFiles,ver);
+
+        });
+    } else {
+        commitDetails(ver);
+    }
+
+};
+var config = function (listOfFiles,optionalVersion) {
+    called(listOfFiles,optionalVersion)
+};
+
+module.exports =  {
+    changeVersion:changeVersion,
+    commitDetails:commitDetails,
+    //COMMAND: @params: version [OPTIONAL:IF NOT PASSED IT WILL AUTO INCREMENT - 5.0.5]
+    //example: Called Manually for changeVersion and commitDetails
+        /*
+            var csVersion = require('cs-version');
+            csVersion.changeVersion('../cs-config.json',params[3],function(v){
+                csVersion.changeVersion('package.json',params[3],function(version){
+                    csVersion.commitDetails(version);
+                    console.info('version added: ',version);
+                });
+            });
+         */
+    run:config
+    //csVersion.run(['../cs-config.json','package.json'],params[3]);
+};
